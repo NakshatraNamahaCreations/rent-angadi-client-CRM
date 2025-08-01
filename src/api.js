ee2a70482d4c39b4ea70aef978c509422102682d
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2025-08-01 12:17:23 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,8 @@
+// export const ApiURL = "https://api.rentangadi.in/api";
+export const ImageApiURL = "https://api.rentangadi.in";
+
+// export const ApiURL = "http://192.168.1.199:8000/api";
+// export const ImageApiURL = "http://192.168.1.199:8000";
+
+export const ApiURL = "http://localhost:8000/api";
+// export const ImageApiURL = "http://localhost:8000";
